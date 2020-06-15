54ef06e776165a147842321918ed9ad6a5ca0fb1
arthur-assan <arthur.assan@gmail.com>
2020-06-15 05:23:33 +0000

    feat: added staff class

---
+++ CSProject/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CSProject
+{
+    class Staff
+    {
+        //Fields
+        private float hourlyRate;
+        private int hWorked;
+
+        //Properties
+        public float TotalPay { get; protected set; }
+        public float BasicPay { get; private set; }
+        public string NameOfStaff { get; private set; }
+
+        public int HoursWorked
+        {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (hWorked > 0)
+                    hWorked = value;
+                else
+                    hWorked = 0;
+            }
+        }
+
+        // Constructors
+        public Staff(string name, float rate)
+        {
+            NameOfStaff = name;
+            hourlyRate = rate;
+        }
+
+        // Method
+        public virtual void CalculatePay()
+        {
+            Console.WriteLine("Calculating Pay...");
+            BasicPay = hWorked * hourlyRate;
+            TotalPay = BasicPay;
+        }
+
+        public override string ToString()
+        {
+            return "Name of Staff = " + NameOfStaff + "Total Pay =" + TotalPay + "Basic Pay" + BasicPay + "Hourly-Rate = " + hourlyRate + "Hours Worked = " + hWorked;
+        }
+
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
