cca1809ac3faad9f479e7ce4e2acad76427e603a
arthur-assan <arthur.assan@gmail.com>
2020-06-16 09:20:45 +0000

    feat: added all relevant classes

--- CSProject/Program.cs
+++ CSProject/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CSProject
 {
@@ -76,6 +78,74 @@ namespace CSProject
             + BasicPay + "\nAllowance = " + Allowance + "\n\nTotalPay = " + TotalPay;
         }
     }
+
+    class Admin : Staff
+    {
+        //Fields
+        private const float overtimeRate = 15.5f;
+        private const float adminHourlyRate = 30f;
+
+        //Propery
+        public float Overtime { get; private set; }
+
+        //constructor
+        public Admin(string name) : base(name, adminHourlyRate) { }
+
+        //Method
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            if (HoursWorked > 160)
+            {
+                Overtime = overtimeRate * (HoursWorked - 160);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "\nNameOfStaff = " + NameOfStaff
+            + "\nadminHourlyRate = " + adminHourlyRate + "\nHoursWorked = " + HoursWorked
+            + "\nBasicPay = " + BasicPay + "\nOvertime = " + Overtime
+            + "\n\nTotalPay = " + TotalPay;
+        }
+    }
+
+    class FileReader
+    {
+        public List<Staff> ReadFile()
+        {
+            List<Staff> myStaff = new List<Staff>();
+            string[] result = new string[2];
+            string path = "staff.txt";
+            string[] separator = { ", " };
+
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        result = sr.ReadLine().Split(separator, StringSplitOptions.RemoveEmptyEntries);
+                        if(result[1] == "Manager")
+                            myStaff.Add(new Manager(result[0]));
+                        else if(result[1] == "Admin")
+                            myStaff.Add(new Manager(result[0]));
+                    }
+
+                    sr.Close();
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Error: File doesnot exit");
+            }
+
+            return myStaff;
+        }
+    }
+
+
 
 
     class Program
